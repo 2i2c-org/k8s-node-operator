0e147975b1c5e8ba25baf166ba72d83bfe71dcc7
jnywong <jnywong.pro@gmail.com>
2026-09-14 17:51:44 +0100

    refactor: create class and add blocking operation

--- src/k8s_node_operator/operator.py
+++ src/k8s_node_operator/operator.py
@@ -1,56 +1,86 @@
 import kopf
+import logging
 import os
 import yaml
 import google.auth
+import google.api_core
 from google.cloud import container_v1
 from typing import Any
 
-GCP_CLUSTER = os.environ.get("GCP_CLUSTER")
-GCP_MACHINE_TYPE = os.environ.get("GCP_MACHINE_TYPE")
-GCP_NODEPOOL = os.environ.get("GCP_NODEPOOL")
-GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
-GCP_ZONE = os.environ.get("GCP_ZONE")
-GOOGLE_APPLICATION_CREDENTIALS = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+logger = logging.getLogger(__name__)
 
-def get_gcp_config(machine):
-    nodepool_name = (
-        f"projects/{GCP_PROJECT_ID}/locations/{GCP_ZONE}/clusters/"
-        f"{GCP_CLUSTER}/nodePools/{GCP_NODEPOOL}"
-    )
-    credentials, _ = google.auth.default()
-    return nodepool_name, credentials
+class GCPClient:
+    def __init__(self):
+        self.cluster_name = os.environ.get("GCP_CLUSTER")
+        self.machine_type = os.environ.get("GCP_MACHINE_TYPE")
+        self.nodepool = os.environ.get("GCP_NODEPOOL")
+        self.project_name = os.environ.get("GCP_PROJECT_ID")
+        self.zone = os.environ.get("GCP_ZONE") # TODO: add support for regional clusters
+        self.prefix =  f"projects/{self.project_name}/zones/{self.zone}" if self.zone else f"projects/{self.project_name}/region/{self.region}"
+        self.nodepool_name = self.prefix + f"/clusters/{self.cluster_name}/nodePools/{self.nodepool}"
+        self.credentials_file = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+        self.credentials, self.project = google.auth.default()
+        self.client = container_v1.ClusterManagerAsyncClient(
+            credentials=self.credentials
+        )
+        logger.debug(self.credentials.get_cred_info())
 
-async def change_min_node_count(credentials, nodepool_name, num):
-    client = container_v1.ClusterManagerAsyncClient(
-        credentials=credentials
-    )
-    request = container_v1.GetNodePoolRequest(
-        name=nodepool_name
-    )  # Get current autoscaling config
-    response = await client.get_node_pool(request=request)
+    async def __aenter__(self):
+        return self
 
-    config = container_v1.NodePoolAutoscaling(
-        enabled=response.autoscaling.enabled,
-        min_node_count=num,
-        max_node_count=response.autoscaling.max_node_count,
-        location_policy=response.autoscaling.location_policy,
-    )
-    request = container_v1.SetNodePoolAutoscalingRequest(name=nodepool_name, autoscaling=config)
-    await client.set_node_pool_autoscaling(request=request)
-    return response.autoscaling.min_node_count
+    async def __aexit__(self, exc_type, exc, tb):
+        await self.client.transport.close()
+
+    async def get_nodepool(self):
+        request = container_v1.GetNodePoolRequest(
+           name=self.nodepool_name
+        )
+        return await self.client.get_node_pool(request=request)
+
+    async def update_autoscaling_min_node_count(self, min_node_count: int):
+        nodepool = await self.get_nodepool()
+        nodepool_autoscaling = container_v1.NodePoolAutoscaling(
+            enabled = nodepool.autoscaling.enabled,
+            min_node_count = min_node_count,
+            max_node_count = nodepool.autoscaling.max_node_count,
+            location_policy = nodepool.autoscaling.location_policy
+        )
+        request = container_v1.SetNodePoolAutoscalingRequest(
+            name=self.nodepool_name,
+            autoscaling=nodepool_autoscaling
+        )
+        return await self.client.set_node_pool_autoscaling(request=request)
+
+    async def wait_gcp_operation(self, operation_name: str):
+        """
+        Blocking call to wait until operation is completed.
+        """
+        name = '/'.join([self.prefix, "operations", operation_name])
+        logger.debug(f'{name=}')
+        request = container_v1.GetOperationRequest(name=name)
+        while True:
+            response = await self.client.get_operation(request=request)
+            if response.status != container_v1.Operation.Status.DONE:
+                logger.info(f'Operation is {container_v1.Operation.Status(response.status).name}')
+            else:
+                return response
+
 
 @kopf.on.create('nodepoolallocationtarget')
 async def create_fn(spec: kopf.Spec, name: str, namespace: str | None, logger: kopf.Logger, **_: Any) -> None:
-
+    # Parse spec
     min_node_count = spec.get('minimumNodeCount')
     if not min_node_count:
         min_node_count = 0
-
+    # Populate npat template with spec
     path = os.path.join(os.path.dirname(__file__), 'npat_template.yaml')
     tmpl = open(path, 'rt').read()
     text = tmpl.format(name=name, minimumNodeCount=min_node_count)
     data = yaml.safe_load(text)
-
-    # Scale cluster
-    nodepool_name, credentials = get_gcp_config(GCP_MACHINE_TYPE)
-    await change_min_node_count(credentials, nodepool_name, int(data["spec"]["minimumNodeCount"]))
+    # Send nodepool scaling request according to npat spec
+    async with GCPClient() as client:
+        operation = await client.update_autoscaling_min_node_count(int(data["spec"]["minimumNodeCount"]))
+        logger.info(f'npat requested')
+        # Determine if scaling operation is completed
+        response = await client.wait_gcp_operation(operation.name)
+        logger.info(f'{response.progress=}')
